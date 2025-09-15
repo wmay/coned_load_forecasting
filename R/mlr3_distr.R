0912b25e7b074974890aa4fb4f334c6543a2d3c6
William May <wmay@hey.com>
2025-09-15 18:38:59 -0400

    Add pipeop scaling to mlr3_distr

--- R/mlr3_distr.R
+++ R/mlr3_distr.R
@@ -222,3 +222,22 @@ mlr_measures$add("regr.crps", function() MeasureRegrCRPS$new())
 
 # # quick test
 # p$score(msr("regr.crps"))
+
+# needed to scale the Y values
+PipeOpTargetTrafoScaleRangeDistr = R6::R6Class("PipeOpTargetTrafoScaleRangeDistr",
+  inherit = PipeOpTargetTrafoScaleRange,
+  public = list(),
+  private = list(
+    .invert = function(prediction, predict_phase_state) {
+      orig_means = unlist(prediction$distr$getParameterValue('mean'))
+      orig_sds = unlist(prediction$distr$getParameterValue('sd'))
+      new_means = (orig_means - self$state$offset) / self$state$scale
+      new_sds = orig_sds / self$state$scale
+      distrs = data.frame(mean = new_means, sd = new_sds) %>%
+        distr6::VectorDistribution$new(distribution = "Normal",
+                                       params = .)
+      PredictionRegr$new(row_ids = prediction$row_ids,
+                         truth = predict_phase_state$truth, distr = distrs)
+    }
+  )
+)
